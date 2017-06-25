895b81aa0efe4621fb8864e2bbd521919d50d04d
EQwon <bluetwintail@naver.com>
2017-06-26 02:03:12 +0900

    방 만들기까지 함

---
+++ Assets/Scripts/RoomCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCreator : MonoBehaviour
+{
+	public GameObject Room;
+	public int stair;
+	public int roomsInStair;
+	public int maxInRoom;
+
+	private float roomWidth = 15f;
+	private float roomHeight = 10f;
+
+	private void Start()
+	{
+		for(int st = 0; st < stair; st++)
+		{
+			for(int ris = 0; ris < roomsInStair; ris++)
+			{
+				Instantiate(Room, new Vector2 (roomWidth * ris, roomHeight * st), Quaternion.identity);
+			}
+		}
+	}
+}
